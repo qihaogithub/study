a49fa4a742a02259b4968801c412dfd04ef9079a
qihaogithub <286383640@qq.com>
2024-04-19 12:59:38 +0800

    更新我的页

--- JavaScript/menu.js
+++ JavaScript/menu.js
@@ -1,41 +1,33 @@
 // JavaScript function to toggle icons and display layer
 function toggleMenu() {
-    var icon = document.getElementById('menu-icon');
-    var layer = document.getElementById('menu-layer');
-    var isIconMenu = icon.getAttribute('name') === 'application-menu';
+  var icon = document.getElementById('menu-icon');
+  var layer = document.getElementById('menu-layer');
+  var isIconMenu = icon.getAttribute('name') === 'application-menu';
 
-    // Toggle the icon name attribute
-    if (isIconMenu) {
-        icon.setAttribute('name', 'close'); // Assuming 'close' is the name of the other icon
-        layer.style.display = 'block';
-    } else {
-        icon.setAttribute('name', 'application-menu');
-        layer.style.display = 'none';
-    }
+  // Toggle the icon name attribute
+  if (isIconMenu) {
+    icon.setAttribute('name', 'close'); // Assuming 'close' is the name of the other icon
+    layer.style.display = 'block';
+  } else {
+    icon.setAttribute('name', 'application-menu');
+    layer.style.display = 'none';
+  }
 
-    // Create list dynamically if not already created
-    if (!layer.querySelector('dl')) {
-        createList();
-    }
+  // Create list dynamically if not already created
+  if (!layer.querySelector('dl')) {
+    createList();
+  }
 }
 
 // Function to create the list dynamically
 function createList() {
-    var listContent = document.getElementById('listContent');
-    var listItems = `
-    <ul>
-    <li>
+  var listContent = document.getElementById('listContent');
+  var listItems = `
       <h3>目录</h3>
-    </li>
-    <li>
-      <ul>
-        <li><a href="index.html" target="_blank">大bnner和小banner</a></li>
-        <li>待补充</li>
-      </ul>
-    </li>
-  </ul>
+        <p><a href="/index.html" target="_blank">大bnner和小banner</a></p><br>
+        <p><a href="/pages/my/wodeye.html" target="_blank">我的页</a></p>
 `;
-    listContent.innerHTML = listItems;
+  listContent.innerHTML = listItems;
 }
 
 // Attach toggleMenu function to the icon click event
